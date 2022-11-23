c686e5712df26374ac0605483db4b9be39a00525
Yuan-San <yuan.arib@gmail.com>
2022-11-24 05:10:15 +0700

    ae

--- happibot/urls.py
+++ happibot/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('members/', include('members.urls')),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
 ] 
