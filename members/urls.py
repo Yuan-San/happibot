8b6c43c885a8b6ca13a22dcfcf290df86ebf6b9b
Yuan-san <yuan.arib@gmail.com>
2022-11-28 04:33:27 +0700

    update 1.0

--- members/urls.py
+++ members/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.urls import include, path
 
 from . import views
 
@@ -9,4 +10,7 @@ urlpatterns = [
   path('delete/<int:id>', views.delete, name='delete'),
   path('update/<int:id>', views.update, name='update'),
   path('update/updaterecord/<int:id>', views.updaterecord, name='updaterecord'),
+  path('student/', views.student, name='student'),
+  path('student/show/<str:name>', views.deletemember, name='deletemember'),
+  path('deletemember/memberdeleterequest/<int:id>', views.memberdeleterequest, name='memberdeleterequest')
 ]
--- members/views.py
+++ members/views.py
@@ -5,25 +5,71 @@ from .models import Members
 
 def index(request):
   mymembers = Members.objects.all().values()
+  happy = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members GROUP BY mood ORDER BY moodcount desc;')
+  total = Members.objects.raw('SELECT 1 id, COUNT(*) AS total FROM members_members;')
+  """
+  sad = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Sad GROUP BY mood;')
+  neutral = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Neutral GROUP BY mood;')
+  surprised = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Surprised GROUP BY mood;')
+  scared = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Scared GROUP BY mood;')
+  disgust = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Disgust GROUP BY mood;')
+  anger = Members.objects.raw('SELECT 1 id, COUNT(mood) AS moodcount, mood FROM members_members WHERE mood = Angry GROUP BY mood;')"""
   template = loader.get_template('index.html')
   context = {
-    'mymembers': mymembers
+    'mymembers': mymembers,
+    'happy': happy,
+    'total': total,
   }
+  """'sad': sad,
+    'neutral': neutral,
+    'surprised': surprised,
+    'scared': scared,
+    'disgust': disgust,
+    'anger': anger"""
   return HttpResponse(template.render(context, request))
   
 def add(request):
   template = loader.get_template('add.html')
   return HttpResponse(template.render({}, request))
+
+def student(request):
+  mymembers = Members.objects.all().values()
+  studentcount = Members.objects.raw('SELECT 1 id, name, COUNT(name) AS count, level, mood, reason FROM members_members GROUP BY name ORDER BY name asc;')
+  template = loader.get_template('student.html')
+  context = {
+    'mymembers': mymembers,
+    'studentcount': studentcount
+  }
+  return HttpResponse(template.render(context, request))
   
 def addrecord(request):
     first = request.POST['first']
     level = request.POST['level']
     mood = request.POST['mood']
     reason = request.POST['reason']
-
     member = Members(name=first, level=level, mood=mood, reason=reason)
     member.save()
     return HttpResponseRedirect(reverse('index'))
+
+def deletemember(request, name):
+    membername = request.session.get('membername')
+    logs = Members.objects.raw('SELECT id, name, mood, reason FROM members_members WHERE name = %s',[name])
+    myname = Members.objects.raw('SELECT id, name, COUNT(name) AS namecount, level FROM members_members WHERE name = %s GROUP BY name', [name])
+    membername = name
+    request.session['membername'] = membername
+    template = loader.get_template('eachstudent.html')
+    context = {
+      'logs':logs,
+      'myname':myname
+    }
+    return HttpResponse(template.render(context, request))
+
+def memberdeleterequest(request, id):
+    membername = request.session.get('membername')
+    member = Members.objects.get(id=id)
+    member.delete()
+    url = reverse('deletemember', kwargs={'name': membername})
+    return HttpResponseRedirect(url)
 
 def delete(request, id):
   member = Members.objects.get(id=id)
